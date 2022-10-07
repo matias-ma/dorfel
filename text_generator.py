6aafcc8ffddaeeb47a7bcae1686e9e0ba0a33402
Matias Andia <107262921+matma14@users.noreply.github.com>
2022-10-07 04:19:35 -0400

    Add files via upload

---
+++ text_generator.py
@@ -0,0 +1,172 @@
+# python3
+
+import tensorflow as tf
+import numpy as np
+import os
+import time
+
+author = input('\n\n Choose author (answer List to list): \n')
+author_list = ['Murakami', 'Nabokov']
+
+if author == 'List':
+    for i in range(len(author_list)):
+        print(author_list[i])
+    author = input('\n Choose author: \n')
+
+start_word = input('\n Give it a word or two to start with \n')
+
+char_count = input('\n How many characters? \n')
+
+if author == author_list[0]:
+    with open('/Users/student/Downloads/many_murakami.txt') as f:
+        text = f.read()
+
+if author == author_list[1]:
+    with open('/Users/student/Downloads/many_nabokov.txt') as f:
+        text = f.read()
+
+vocab = sorted(set(text))
+example_texts = ['abcdefg', 'xyz']
+
+chars = tf.strings.unicode_split(example_texts, input_encoding='UTF-8')
+ids_from_chars = tf.keras.layers.StringLookup(
+    vocabulary=list(vocab), mask_token=None)
+    
+ids = ids_from_chars(chars)
+chars_from_ids = tf.keras.layers.StringLookup(
+    vocabulary=ids_from_chars.get_vocabulary(), invert=True, mask_token=None)
+
+chars = chars_from_ids(ids)
+tf.strings.reduce_join(chars, axis=-1).numpy()
+def text_from_ids(ids):
+  return tf.strings.reduce_join(chars_from_ids(ids), axis=-1)
+
+all_ids = ids_from_chars(tf.strings.unicode_split(text, 'UTF-8'))
+ids_dataset = tf.data.Dataset.from_tensor_slices(all_ids)
+
+
+seq_length = 100
+
+sequences = ids_dataset.batch(seq_length+1, drop_remainder=True)
+
+BATCH_SIZE = 2048
+BUFFER_SIZE = 10000
+
+def split_input_target(sequence):
+    input_text = sequence[:-1]
+    target_text = sequence[1:]
+    return input_text, target_text
+
+dataset = sequences.map(split_input_target)
+
+
+dataset = (
+    dataset
+    .shuffle(BUFFER_SIZE)
+    .batch(BATCH_SIZE, drop_remainder=True)
+    .prefetch(tf.data.experimental.AUTOTUNE))
+
+# Length of the vocabulary in StringLookup Layer
+vocab_size = len(ids_from_chars.get_vocabulary())
+
+# The embedding dimension
+embedding_dim = 256
+
+# Number of RNN units
+rnn_units = 1024
+
+class MyModel(tf.keras.Model):
+  def __init__(self, vocab_size, embedding_dim, rnn_units):
+    super().__init__(self)
+    self.embedding = tf.keras.layers.Embedding(vocab_size, embedding_dim)
+    self.gru = tf.keras.layers.GRU(rnn_units,
+                                   return_sequences=True,
+                                   return_state=True)
+    self.dense = tf.keras.layers.Dense(vocab_size)
+
+  def call(self, inputs, states=None, return_state=False, training=False):
+    x = inputs
+    x = self.embedding(x, training=training)
+    if states is None:
+      states = self.gru.get_initial_state(x)
+    x, states = self.gru(x, initial_state=states, training=training)
+    x = self.dense(x, training=training)
+
+    if return_state:
+      return x, states
+    else:
+      return x
+
+model = MyModel(
+    vocab_size=vocab_size,
+    embedding_dim=embedding_dim,
+    rnn_units=rnn_units)
+
+for input_example_batch, target_example_batch in dataset.take(1):
+    example_batch_predictions = model(input_example_batch)
+
+
+class OneStep(tf.keras.Model):
+  def __init__(self, model, chars_from_ids, ids_from_chars, temperature=1.0):
+    super().__init__()
+    self.temperature = temperature
+    self.model = model
+    self.chars_from_ids = chars_from_ids
+    self.ids_from_chars = ids_from_chars
+
+    # Create a mask to prevent "[UNK]" from being generated.
+    skip_ids = self.ids_from_chars(['[UNK]'])[:, None]
+    sparse_mask = tf.SparseTensor(
+        # Put a -inf at each bad index.
+        values=[-float('inf')]*len(skip_ids),
+        indices=skip_ids,
+        # Match the shape to the vocabulary
+        dense_shape=[len(ids_from_chars.get_vocabulary())])
+    self.prediction_mask = tf.sparse.to_dense(sparse_mask)
+
+  @tf.function
+  def generate_one_step(self, inputs, states=None):
+    # Convert strings to token IDs.
+    input_chars = tf.strings.unicode_split(inputs, 'UTF-8')
+    input_ids = self.ids_from_chars(input_chars).to_tensor()
+
+    # Run the model.
+    # predicted_logits.shape is [batch, char, next_char_logits]
+    predicted_logits, states = self.model(inputs=input_ids, states=states,
+                                          return_state=True)
+    # Only use the last prediction.
+    predicted_logits = predicted_logits[:, -1, :]
+    predicted_logits = predicted_logits/self.temperature
+    # Apply the prediction mask: prevent "[UNK]" from being generated.
+    predicted_logits = predicted_logits + self.prediction_mask
+
+    # Sample the output logits to generate token IDs.
+    predicted_ids = tf.random.categorical(predicted_logits, num_samples=1)
+    predicted_ids = tf.squeeze(predicted_ids, axis=-1)
+
+    # Convert from token ids to characters
+    predicted_chars = self.chars_from_ids(predicted_ids)
+
+    # Return the characters and model state.
+    return predicted_chars, states
+
+one_step_model = OneStep(model, chars_from_ids, ids_from_chars)
+
+if author == author_list[0]:
+    model.load_weights('/Users/student/Desktop/tf_models/many_murakami.h5')
+
+if author == author_list[1]:
+    model.load_weights('/Users/student/Desktop/tf_models/many_nabokov.h5')
+
+states = None
+next_char = tf.constant([start_word])
+result = [next_char]
+
+for n in range(int(char_count)):
+  next_char, states = one_step_model.generate_one_step(next_char, states=states)
+  result.append(next_char)
+
+result = tf.strings.join(result)
+
+print('\n\n'+result[0].numpy().decode('utf-8'), '\n\n' + '_'*80)
+
